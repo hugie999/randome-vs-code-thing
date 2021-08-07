7a71525489bbc30103b4330aedc24a688a6c6c2f
hugie999 <88388675+hugie999@users.noreply.github.com>
2021-08-07 10:28:40 -0700

    pp

---
+++ game thing.py
@@ -0,0 +1,57 @@
+from io import open_code
+
+
+
+# def guess(number):
+    # print ("question ") (number)
+    # print (" ")
+    # print ("what is ") (number * 5) ("+") (number +3)
+    # 
+    # qu2 = (number * 5 + number+3)
+    # 
+    # qu = input (":")
+    # 
+    # if qu == qu2:
+        # print ("good job")
+        # 
+    # else:
+        # 
+        # print ("to bad you lost :(")
+    # quit()
+    
+    
+
+print ("hello world")
+print ("want to start game")
+
+qu = input("(Y/N):")
+
+if qu == ("Y") or qu == ("y"):
+
+    print (" ")
+    print (" ")
+    print  ("hugo game thing")
+    print ("version: 1.0")
+    print ("did thing")
+    print (" ")
+    
+    print ("--hugo's guessing game--")
+    print ("----------v1.0----------")
+    input (":")
+    print (" ")
+    # guess(1)
+    print ("is canada a?")
+    print ("democracy (1) or dictator ship (2)")
+    qu = input(":")
+    if qu == ("1"):
+        print ("good job")
+
+    elif qu == ("2"):
+    
+        print ("incorect")
+    
+    else:
+        print("error [100]")
+        exit()
+elif qu == ("N") or qu == ("n"):
+    print ("ok :(")
